e41563ab923b91ea08dec1f101fe169eac16e319
Leonardo Bezerra da Silva <165049879+H4ttiz@users.noreply.github.com>
2026-03-26 18:12:16 -0300

    feat: adiciona nível de usuário, hierarquia e campo CPF (#31)
    * Develop (#17)
    
    * feat: adiciona configuração inicial do projeto, dependências Maven e teste de conexão com PostgreSQL.
    
    * feat: configura estrutura inicial do projeto com Maven e conexão JDBC
    
    * config: ajusta CI, conexão e testes
    
    * Feature: Implementação Model e DAO de Usuarios
    
    * Atualizando Usuario
    
    * Finalizando Cadastro de Usuarios
    
    * Arrumando a classe EmailUtil
    
    * Add CODEOWNERS file with H4ttiz as owner
    
    * Feature/cadastro usuario (#15)
    
    * Arrumando O CI do github
    
    * Arrumando o .bat
    
    * Feature/cadastro usuario (#16)
    
    * Arrumando O CI do github
    
    * Arrumando o .bat
    
    * remove .idea
    
    * feat: adiciona nível de usuário, hierarquia e campo CPF

--- src/main/java/br/com/observaacao/model/usuario/TipoUsuario.java
+++ src/main/java/br/com/observaacao/model/usuario/TipoUsuario.java
@@ -3,7 +3,8 @@ package br.com.observaacao.model.usuario;
 public enum TipoUsuario {
     C("Cidadão"),
     S("Servidor Público"),
-    G("Gestor");
+    G("Gestor"),
+    A("Administrador");
 
     private String descricao;
 
--- target/classes/db/V1__create_tables.sql
+++ target/classes/db/V1__create_tables.sql
@@ -1,9 +1,35 @@
 CREATE TABLE usuarios (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    nome VARCHAR(150) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    tipo_usuario VARCHAR(1) NOT NULL,
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    ativo BOOLEAN DEFAULT TRUE
+    id bigserial  NOT NULL,
+    criado_por bigserial  NULL,
+    nome varchar(150) NOT NULL,
+    cpf bpchar(11) NOT NULL,
+    email varchar(150) NOT NULL,
+    senha varchar(255) NOT NULL,
+    tipo_usuario bpchar(1) NOT NULL,
+    data_criacao timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+    ativo bool DEFAULT true NULL,
+    CONSTRAINT usuarios_email_key UNIQUE (email),
+    CONSTRAINT usuarios_pk PRIMARY KEY (id),
+    CONSTRAINT usuarios_unique UNIQUE (cpf),
+    CONSTRAINT fk_criado_por FOREIGN KEY(criado_por) REFERENCES usuarios(id)
+);
+CREATE TABLE categorias (
+    id bigserial  NOT NULL,
+    categoria varchar(150) NOT NULL,
+    descricao text NOT NULL,
+    data_criacao timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    ativo bool DEFAULT true NOT NULL,
+    CONSTRAINT categorias_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE enderecos (
+    id bigserial NOT NULL,
+    cep varchar(10) NOT NULL,
+    logradouro varchar(150) NOT NULL,
+    numero varchar(10) NOT NULL,
+    complemento varchar(100) NULL,
+    bairro varchar(100) NOT NULL,
+    cidade varchar(100) NOT NULL,
+    estado varchar(2) NOT NULL,
+    CONSTRAINT enderecos_pk PRIMARY KEY (id)
 );
