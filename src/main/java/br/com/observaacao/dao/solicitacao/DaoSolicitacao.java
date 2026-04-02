30d9c4675a40c02a754201560fee770c1d834ad5
Carlos Eduardo Carfi <157553755+CaduCarfi@users.noreply.github.com>
2026-04-02 17:50:09 -0300

    Feature/cadastro solicitacao (#34)
    * Develop (#17)
    
    * feat: adiciona configuração inicial do projeto, dependências Maven e teste de conexão com PostgreSQL.
    
    * feat: configura estrutura inicial do projeto com Maven e conexão JDBC
    
    * config: ajusta CI, conexão e testes
    
    * Feature: Implementação Model e DAO de Usuarios
    
    * Atualizando Usuario
    
    * Finalizando Cadastro de Usuarios
    
    * Arrumando a classe EmailUtil
    
    * Add CODEOWNERS file with H4ttiz as owner
    
    * Feature/cadastro usuario (#15)
    
    * Arrumando O CI do github
    
    * Arrumando o .bat
    
    * Feature/cadastro usuario (#16)
    
    * Arrumando O CI do github
    
    * Arrumando o .bat
    
    * feat: cria solicitação no banco de dados, Dao, Model e Service
    
    ---------
    
    Co-authored-by: Leonardo Bezerra da Silva <165049879+H4ttiz@users.noreply.github.com>

---
+++ src/main/java/br/com/observaacao/dao/solicitacao/DaoSolicitacao.java
@@ -0,0 +1,7 @@
+package br.com.observaacao.dao.solicitacao;
+
+import br.com.observaacao.dao.DaoGenerico;
+import br.com.observaacao.model.solicitacao.Solicitacao;
+
+public interface DaoSolicitacao extends DaoGenerico<Solicitacao> {
+}
